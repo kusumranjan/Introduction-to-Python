2673b8bb867ecd850a75a4cd04db739d62525e4e
kusumranjan <Kusum.Ranjan@gds.ey.com>
2025-11-24 15:08:06 +0530

    Create basic code.py

---
+++ basic code.py
@@ -0,0 +1,45 @@
+name = "Kusum Ranjan"
+age = 22
+salary = 4.8
+
+print(name)
+print(age)
+print(salary)
+# arthematic
+a = 10
+b = 3
+print("addition",a+b)
+print("division ",a/b)
+print("floor divison " , a%b)
+print('power',a**b)
+
+#list number till 20
+
+
+for i in range(1, 21):
+    if i % 2:  
+        print(i, "is odd")
+    else:      
+        print(i, "is even")
+ # elif       
+marks = int(input("enter your score:"))
+
+if marks >=90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 60:
+    print("Grade C")
+else:
+    print("Grade D")
+# for loop    
+for i in range(1,6):
+    print("Number:", i)
+#while loop iterator    
+    
+counter = 1
+
+
+while counter <= 5:
+    print("count:", counter)
+    counter += 1
