233a5772a33fe1cf0a6aa49c52293273468d6b8a
kusumranjan <Kusum.Ranjan@gds.ey.com>
2025-12-02 01:42:55 +0530

    Create five_lines_in_a_file.py

---
+++ 27-11-2025/five_lines_in_a_file.py
@@ -0,0 +1,6 @@
+with open ("notes.txt", "w") as f:
+    f.write("Hello world \n")
+    f.write("Hi how are you \n")
+    f.write("I like to eat fruits. \n")
+    f.write("I also like things that contain cocoa and are less sweet")
+    f.close()
